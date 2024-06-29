27b431bb7a084c43df0a855dd9db659d7cfba59a
Juliano Pereira <juliano340@gmail.com>
2024-06-29 16:48:35 -0300

    feat: Ajustes visuais na interface
    - Adicionado ícone.
    - Ajuste visual no botão voltar.
    - Adicionado ícones aos cards da index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faBox } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
@@ -8,13 +10,19 @@ const Home = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <Link href="/clientes" legacyBehavior>
             <a className="block p-6 bg-[#233243] rounded-lg shadow-lg hover:bg-[#4c5b6e] transition">
-              <h2 className="text-xl font-bold mb-2 text-white">Gerenciar Clientes</h2>
+              <div className="flex items-center mb-2">
+                <FontAwesomeIcon icon={faUsers} className="text-white mr-2" size="lg" />
+                <h2 className="text-xl font-bold text-white">Gerenciar Clientes</h2>
+              </div>
               <p className="text-gray-300">Acesse para adicionar, editar ou remover clientes.</p>
             </a>
           </Link>
           <Link href="/produtos" legacyBehavior>
             <a className="block p-6 bg-[#233243] rounded-lg shadow-lg hover:bg-[#4c5b6e] transition">
-              <h2 className="text-xl font-bold mb-2 text-white">Gerenciar Produtos</h2>
+              <div className="flex items-center mb-2">
+                <FontAwesomeIcon icon={faBox} className="text-white mr-2" size="lg" />
+                <h2 className="text-xl font-bold text-white">Gerenciar Produtos</h2>
+              </div>
               <p className="text-gray-300">Acesse para adicionar, editar ou remover produtos.</p>
             </a>
           </Link>
